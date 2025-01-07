df21b9fef28bded5936001e35943394ae3b8ae72
EAL <elias@takefive.dk>
2025-01-07 03:54:23 +0100

    Removed old function code

--- src/LeGUILib.cpp
+++ src/LeGUILib.cpp
@@ -39,17 +39,3 @@ void LeGUILib::updateDirtyElements()
         elementsForDrawing_[elementIndices] = weakElements_[elementIndices].lock()->clone();
     }
 }
-
-/*
-template <typename T>
-std::shared_ptr<T> LeGUILib::createElement()
-{
-    auto element = std::make_shared<T>();
-    static_cast<GUIElement>(element).setElementUpdater(elementUpdater_);
-    static_cast<GUIElement>(element).setID(static_cast<int>(weakElements_.size()));
-    weakElements_.emplace_back(element);
-
-    return element;
-}
-
-*/
