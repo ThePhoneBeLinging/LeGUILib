fcdcd1ee02ec9e2eae68e8324b056a57c8565020
EAL <elias@takefive.dk>
2025-04-07 22:11:54 +0200

    fix: added HideCursor to LeGuiLib();

--- src/LeGUILib.cpp
+++ src/LeGUILib.cpp
@@ -12,6 +12,7 @@ LeGUILib::LeGUILib() : keepRunning_(true)
 {
     InitWindow(1280, 720, "LeGUI");
     ToggleFullscreen();
+    HideCursor();
     SetTargetFPS(10);
     //eventController_ = std::make_shared<EventController>();
     //eventListener_ = std::make_unique<EventListener>("/dev/input/by-path/platform-1f00080000.i2c-event",eventController_);
